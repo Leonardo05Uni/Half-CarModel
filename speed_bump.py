49cfbe50ad258d9b86fcab96e03dfbcab49e42a4
s2643058 <s2643058@ed.ac.uk>
2025-10-08 21:57:03 +0100

    Created function for calculating height of speed bump at location x.

--- speed_bump.py
+++ speed_bump.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+_H = 0.5 # Maximum height of speed bump
+_B = 2 # Maximum length of speed bump
+
+def speed_bump_height(x):
+    if (x>=0) and (x<=_B): # Checks if position is within length of speed bump
+        height = (_H/2)*(1 - np.cos(2*np.pi*x/_B)) # Formula for speed bump
+    else:
+        height = 0 # 0 if out of range 
+
+    return height # output of the height of the speed bump at that point (x)
