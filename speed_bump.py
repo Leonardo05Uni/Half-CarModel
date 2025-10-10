02558af2215a00b50b552a53b7ee96da18fdc2af
BryanXiao1 <s2522126@ed.ac.uk>
2025-10-11 01:17:05 +0800

    Update speed_bump.py

--- speed_bump.py
+++ speed_bump.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt # Importing matplotlib for plotting graphs
 bump_H = 0.5 # Maximum height of speed bump
 bump_L = 2 # Maximum length of speed bump
 
-margin=bump_L*0.75
-x_center=bump_L/2
-x=np.linspace(-margin,bump_L+margin,400)
-h=np.where((x>=0)&(x<=bump_L), 0.5*bump_H*(1-np.cos(2*np.pi*x/bump_L)),0)
-plt.figure(figsize=(bump_H+5,bump_L+5))
-plt.plot(x,h,color='black',linewidth=1)
-plt.xlim(x_center-margin, x_center+margin)
-plt.ylim(0,5)
+margin=bump_L*0.75 # the margin on each side of the plot for good looking
+x_center=bump_L/2 # centre of the speed bump
+x=np.linspace(-margin,bump_L+margin,400) # range of x axis, 400 displays the amount of dots
+h=np.where((x>=0)&(x<=bump_L), 0.5*bump_H*(1-np.cos(2*np.pi*x/bump_L)),0) # function of speed bump
+plt.figure(figsize=(bump_H+5,bump_L+5)) # figure size that displayed
+plt.plot(x,h,color='black',linewidth=1) # color and linewidth of the graph
+plt.xlim(x_center-margin, x_center+margin) # centre the plot, range of x axis
+plt.ylim(0,5) # range of y axis
 plt.tight_layout()
-plt.axis("off")
+plt.axis("off") # hide the axis, make it clearer
 plt.show()
 
 def speed_bump_height(x):
